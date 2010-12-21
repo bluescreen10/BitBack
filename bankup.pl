5a7e2fd811021f5d0c767b4c25eca5cc4d37c521
bluescreen10 <dichoso@gmail.com>
2010-12-20 22:27:22 -0300

    Start working in the GUI

---
+++ bankup.pl
@@ -0,0 +1,43 @@
+#! /usr/bin/perl
+
+use strict;
+use Gtk2 '-init';
+use Data::Dumper;
+
+my $builder = Gtk2::Builder->new();
+$builder->add_from_file('./src/gtkbuilder/bankup.glade');
+
+my $window = $builder->get_object("main_window");
+$builder->connect_signals(undef);
+
+$window->show();
+
+Gtk2->main();
+
+exit;
+
+sub cb_destroy {
+    print STDERR "Hello\n";
+    Gtk2->main_quit();
+
+}
+
+sub cb_clicked {
+    my $progress_bar = $builder->get_object("progressbar1");
+    $progress_bar->set_fraction(0.5);
+    my $model = $builder->get_object("profiles_store");
+    $model->insert_with_values( 3, 0 => "New", 1 => "None", 2 => "", 3 => ""  );
+
+}
+
+sub cb_main_window_about_clicked {
+   my $dialog = $builder->get_object("about_dialog");
+   $dialog->show;
+}
+
+sub cb_main_window_profile_selected {
+    print Dumper(@_);
+}
+
+Gtk2->main;
+
